949c27272c3dba4e074333a0727b76a9e0dd55d5
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2025-05-13 18:14:25 -0500

    Creacion de mas controladores

---
+++ src/main/java/co/edu/uniquindio/reservasfx/controladores/Cliente1Controlador.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.reservasfx.controladores;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Cliente1Controlador {
+
+    @FXML
+    private Label estrellasLbl;
+
+    @FXML
+    private Label nombreLbl;
+
+    @FXML
+    private Label comentariosLbl;
+}
---
+++ src/main/java/co/edu/uniquindio/reservasfx/controladores/CostoAdicionalControlador.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.reservasfx.controladores;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CostoAdicionalControlador {
+
+    @FXML
+    private Label costoAseoMantenimientoLabel;
+
+    @FXML
+    private Label tipoAlojamientoLabel;
+}
