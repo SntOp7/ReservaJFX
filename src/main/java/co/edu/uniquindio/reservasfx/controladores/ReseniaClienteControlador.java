7f1534c8b62473bcf5def179918fca14253b6f0a
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2025-05-14 21:30:59 -0500

    Updaate

--- src/main/java/co/edu/uniquindio/reservasfx/controladores/ReseniaClienteControlador.java
+++ src/main/java/co/edu/uniquindio/reservasfx/controladores/ReseniaClienteControlador.java
@@ -1,8 +1,12 @@
 package co.edu.uniquindio.reservasfx.controladores;
 
 import co.edu.uniquindio.reservasfx.modelo.entidades.Calificacion;
+import co.edu.uniquindio.reservasfx.modelo.entidades.usuario.Cliente;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class ReseniaClienteControlador {
 
@@ -15,8 +19,14 @@ public class ReseniaClienteControlador {
     @FXML
     private Label comentariosLbl;
 
+    ControladorPrincipal controlador = ControladorPrincipal.getInstancia();
+
+    private ArrayList<Cliente> clientes = controlador.getEmpresa().getModuloUsuarioServicios().getUsuarioServicios().obtenerClientes();
+
     public void inicializarValores(Calificacion calificacion) {
-        nombreLbl.setText(calificacion.getCliente().getNombre()); // o lo que uses para nombre
+        String cedula = calificacion.getCedulaCliente();
+        String nombre = clientes.stream().filter(a -> a.equals(cedula)).findFirst().get().getNombre();
+        nombreLbl.setText(nombre);
         estrellasLbl.setText(String.valueOf(calificacion.getValoracion()));
         comentariosLbl.setText(calificacion.getComentario());
     }
